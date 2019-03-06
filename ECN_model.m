9a32753ea593467a729ff076d74b9b205a1913f1
vbaranchikov <vbaranchikov@users.noreply.github.com>
2019-03-06 17:52:49 +0000

    Still not really working

--- ECN_model.m
+++ ECN_model.m
@@ -1,9 +1,11 @@
-function [V] = ECN_model(params,I)
+% function [V] = ECN_model(params,time)
+  function [V] = ECN_model(params,I)
 %ECN_MODEL Summary of this function goes here
 %   Detailed explanation goes here
 
 global OCV_prof
 global SoC_prof
+% global I
 
 R0 = params(1);
 R1 = params(2);
@@ -17,7 +19,7 @@ V = zeros(length(I),1);
 
 i_r1(1) = 0; %Assume steady state start
 SoC(1) = SoC_init;
-dt = 1;
+dt = 0.1;
 V(1) = OCV_prof(find(SoC_prof<SoC(1), 1));
 % V(1) = interp1(SoC_prof, OCV_prof, SoC(1)) - R0*I(1);
 
@@ -26,7 +28,10 @@ for i = 2:length(I)
     
     i_r1(i) = exp(-dt/(R1*C1))*i_r1(i-1) +...
         (1 - exp(-dt/(R1*C1)))*I(i-1);
+    
 %     OCV = interp1(SoC_prof, OCV_prof, SoC(i));
     OCV = OCV_prof(find(SoC_prof<SoC(i), 1));
-    V(i) = OCV - R1*i_r1(i) - R0*I(i);
+    V(i) = OCV + R1*i_r1(i) + R0*I(i);
 end
+
+save('SoC.mat', 'SoC');
--- Proj3_part2.m
+++ Proj3_part2.m
@@ -4,6 +4,7 @@ close all
 
 global OCV_prof
 global SoC_prof
+global I
 
 load('OCV_data.mat')
 
@@ -23,11 +24,35 @@ n_SoC = 8;
 n_pulses = 8;
 pulse_idx = 1;
 
+R0_init = 8.2e-3;
+R1_init = 15.8e-3;
+C1_init = 38e3;
+
 for i = 1:n_SoC
     SoC(i) = SoC_prof(find(OCV_prof < VoltageV1(pulse_locs(pulse_idx)-1), 1));
     %     SoC(i) = interp1(OCV_prof(1:end-5), SoC_prof(1:end-5), VoltageV1(pulse_locs(pulse_idx)-1));
     for j = 1:n_pulses
         disp(['Iteration: ' num2str(i) ', ' num2str(j)])
+        
+        if j == n_pulses
+            
+            I = CurrentA(pulse_locs(pulse_idx)-10:pulse_locs(pulse_idx)+10000);
+            
+            params{i,j} = lsqcurvefit(@ECN_model,[R0_init,R1_init,C1_init,SoC(i)],...
+                1:1:10010,...
+                VoltageV1(pulse_locs(pulse_idx)-10:pulse_locs(pulse_idx)+10000),...
+                [10e-3,10e-3,1e3,SoC(i)],[100e-3,100e-3,inf,SoC(i)]);
+        else
+            
+            I = VoltageV1(pulse_locs(pulse_idx)-10:pulse_locs(pulse_idx+1)-1);
+            
+            params{i,j} = lsqcurvefit(@ECN_model,[R0_init,R1_init,C1_init,SoC(i)],...
+                1:1:length(VoltageV1(pulse_locs(pulse_idx)-10:pulse_locs(pulse_idx+1)-1)) ,...
+                VoltageV1(pulse_locs(pulse_idx)-10:pulse_locs(pulse_idx+1)-1),...
+                [10e-3,10e-3,1e3,SoC(i)],[100e-3,100e-3,inf,SoC(i)]);
+        end
+        
+        %{
         if j == n_pulses
             params{i,j} = lsqcurvefit(@ECN_model,[0.01,0.01,1e-7,SoC(i)],...
                 CurrentA(pulse_locs(pulse_idx)-1:pulse_locs(pulse_idx)+10000) ,...
@@ -39,6 +64,8 @@ for i = 1:n_SoC
                 VoltageV1(pulse_locs(pulse_idx)-1:pulse_locs(pulse_idx+1)-1),...
                 [0,0,0,SoC(i)],[inf,inf,inf,SoC(i)]);
         end
+        
+        %}
         pulse_idx = pulse_idx + 1;
         
     end
